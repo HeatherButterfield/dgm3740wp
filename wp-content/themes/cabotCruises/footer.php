9178c03285cf8792c96a6d652cf2eaf25adcbf7e
HeatherHyer <heather.hyer@familysearch.org>
2020-04-14 13:36:26 -0600

    header footer

--- wp-content/themes/cabotCruises/footer.php
+++ wp-content/themes/cabotCruises/footer.php
@@ -1,2 +1,84 @@
+<div class="footerWrapper">
+  <footer>
+      <div class="footerSection">
+        <h3>Contact Us</h3>
+        <a href="https://www.google.com/maps/place/Classic+Skating+%26+Fun+Center/@40.2931898,-111.697047,17z/data=!3m1!4b1!4m5!3m4!1s0x874d9ac6daee9bbb:0xcd106807b51c2a60!8m2!3d40.2931898!4d-111.6948583">250 State St, Orem, UT 84058</a>
+        <h3>Phone</h3>
+        <a id="headerTel" href="tel:123-456-7890">123-456-7890</a>
+        <h3>Hours of Operation</h3>
+        <p>Monday-Friday  9am-5pm</p>
+      </div>
+      <div class="footerSection">
+        <h3>Newsletter</h3>
+        <p>Subscribe to our newsletter to stay up to date with our amazing deals</p>
+        <form>
+          <input type="email" name="email" required>
+          <button type="submit" name="submit">Submit</button>
+        </form>
+      </div>
+      <div class="footerSection">
+        <h3>Why Cabot Cruises?</h3>
+        <p>Cabot Cruises is a travel agency providing the best cruise deals on our online travel website. We can help you with all inclusive vacations including discounted cruises.</p>
+      </div>
+  </footer>
+  <div class="social">
+    <div class="socialSection">
+      <i class="fab fa-facebook-f fa-3x"></i>
+      <i class="fab fa-instagram fa-3x"></i>
+      <i class="fab fa-twitter fa-3x"></i>
+      <i class="fab fa-linkedin-in fa-3x"></i>
+      <i class="fab fa-pinterest-p fa-3x"></i>
+    </div>
+    <p class="copy">&copy; Heather Butterfield 2020 <a href="https://heatherbutterfield.com/portfolio/">Portfolio</a></p>
+  </div>
+</div>
+
+<script type="text/javascript" src="./js/toggleMenu.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="./slick/slick.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+  $('.slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    autoplay: true
+  });
+});
+const href = window.location.href.split('/');
+const page = href[href.length-1];
+if (page === 'index.php') {
+  document.querySelector('.home').classList.add('current-menu-item');
+}
+else if (page === 'cruise.php') {
+  document.querySelector('.cruise').classList.add('current-menu-item');
+}
+else if (page === 'book.php') {
+  document.querySelector('.book').classList.add('current-menu-item');
+}
+else if (page === 'agent1.php') {
+  document.querySelector('.agentMain').classList.add('current-menu-item');
+}
+else if (page === 'agent2.php') {
+  document.querySelector('.agentMain').classList.add('current-menu-item');
+}
+</script>
+<script type="application/ld+json">
+{
+"@context": "https://schema.org",
+"@type": "LocalBusiness",
+"name": "Cabot Cruises",
+"address": {
+  "@type": "PostalAddress",
+  "streetAddress": "250 State St",
+  "addressLocality": "Orem",
+  "addressRegion": "UT",
+  "postalCode": "84058",
+  "addressCountry": "US"
+},
+"telephone": "1234567890",
+"openingHours": "Mo-Fr 09:00-17:00"
+}
+</script>
 </body>
 </html>
--- wp-content/themes/cabotCruises/header.php
+++ wp-content/themes/cabotCruises/header.php
@@ -2,8 +2,27 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Cabot Cruises <?php wp_title(); ?></title>
+  <title>Cabot Cruises <?php wp_title(); ?></title>  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+  <link href="<?php echo get_template_directory_uri(); ?>css/styles.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
 
-<?php wp_nav_menu(); ?>
+  <div class="headerWrapper">
+    <header class="clearfix">
+      <figure>
+        <img class="logo" src="./images/logo1.png" alt="logo" srcset="./images/logo1.png 1x, ./images/logo2.png 2x, ./images/logo3.png 3x">
+      </figure>
+      <h1>Cabot Cruises</h1>
+      <a id="headerTel" href="tel:123-456-7890">123-456-7890</a>
+    </header>
+  </div>
+
+  <div class="navWrapper">
+    <nav class="clearfix">
+      <button id="hamburgerBtn"><span><i>&equiv;</i>OPEN</span> <span><i>&otimes;</i>CLOSE</span></button>
+      <?php wp_nav_menu(); ?>
+      <div class="searchBar"><input type="search" id="searchInput" value="Search..."><i class="fas fa-search"></i></div>
+    </nav>
+  </div>
