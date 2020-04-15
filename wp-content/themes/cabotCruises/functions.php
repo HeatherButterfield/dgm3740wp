9f58e7688d94cef7ff7ad0b1d0ba5e8a5ace667a
HeatherHyer <heather.hyer@familysearch.org>
2020-04-15 11:41:41 -0600

    fix

--- wp-content/themes/cabotCruises/functions.php
+++ wp-content/themes/cabotCruises/functions.php
@@ -5,8 +5,8 @@ add_filter('user_can_richedit', '__return_false', 50);
 //add a menu
 register_nav_menu('siteNavigation', 'Primary Navigation');
 
-function updocs_excerpt_more($more) {
-  return '<a href="'.get_the_permalink().'" rel="nofollow">Read more...</a>'
+function wpdocs_excerpt_more( $more ) {
+    return '<a href="'.get_the_permalink().'" rel="nofollow">Read More...</a>';
 }
-add_filter('excerpt_more', 'updocs_excerpt_more');
+add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
 ?>
